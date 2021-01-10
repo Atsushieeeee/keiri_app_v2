322ead54cb620dd705fd51aad61fe70355549094
Atsushi-Endo <ena-choco@live.jp>
2021-01-10 22:01:06 +0900

    サイドバーを仮適用から変更

--- src/components/side.vue
+++ src/components/side.vue
@@ -1,13 +1,13 @@
 <template>
       <div id="nav">
         <ul>
-          <li><router-link to="/contents1">Contents1</router-link></li>
-          <li><router-link to="/contents2">Contents2</router-link></li>
-          <li><router-link to="/contents3">Contents3</router-link></li>
-          <li><router-link to="/contents4">Contents4</router-link></li>
-          <li><router-link to="/contents5">Contents5</router-link></li>
-          <li><router-link to="/contents6">Contents6</router-link></li>
-          <li><router-link to="/contents7">Contents7</router-link></li>
+          <li><router-link to="/contents1">手形分割計算</router-link></li>
+          <li><router-link to="/contents2">預金利息</router-link></li>
+          <li><router-link to="/contents3">上場株式配当金</router-link></li>
+          <li><router-link to="/contents4">非上場株式配当金</router-link></li>
+          <li><router-link to="/contents5">契約書</router-link></li>
+          <li><router-link to="/contents6">源泉</router-link></li>
+          <li><router-link to="/contents7">売上代金に係る金銭又は有価証券の受取書</router-link></li>
         </ul>        
       </div>
 </template>
