098a198d8a62d8111d0ef9a38e19bba2521d1524
Atsushi-Endo <ena-choco@live.jp>
2021-01-10 22:31:16 +0900

    不要なルーティングの削除

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,9 @@
 <template>
   <div class="home">
-    <Top/>
+    TOP
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import Top from '@/components/Top.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    Top
-  }
-}
 </script>
